e2bb9ff8383b23a39bd4d57ee2c78248ba1633b9
CristianoRico <carvalhocristianorico@gmail.com>
2025-02-26 09:26:17 +0700

    commit awal

---
+++ lib/services/api_services.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = "https://api.themovidedb.org/3";
+  static const String apiKey = "d4c377db9c536cc31d55c0749dc999f7";
+
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
+    final response =
+        await http.get(Uri.parse('$baseUrl/movie/now_playing?api_key=$apiKey'));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  Future<List<Map<String, dynamic>>> getTrendingMovies() async {
+    final response = await http
+        .get(Uri.parse('$baseUrl/trending/movie/week?api_key=$apiKey'));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  Future<List<Map<String, dynamic>>> getPopularMovies() async {
+    final response =
+        await http.get(Uri.parse('$baseUrl/movie/popular?api_key=$apiKey'));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+}
